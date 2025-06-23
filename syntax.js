ef5aebbb40fe1facac2088ada6bdb0af3eaa70f4
AkudariArjun <arjunjustin3738@gmail.com>
2025-06-23 15:42:14 +0530

    work

---
+++ syntax.js
@@ -0,0 +1,23 @@
+let a=10;
+//let is a block scoped cant be redeclared in the same scope
+
+const b=20;
+//const is a function scoped,and cannot be re assisted 
+
+
+
+
+
+
+var c=30;
+
+
+
+
+console.log(a,b,c);
+
+
+
+
+
+let  userInput = promt("enetr a number: ");
