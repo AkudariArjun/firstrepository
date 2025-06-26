bb4bc4be585fc972007a2d9de6223bf876e78a3a
AkudariArjun <arjunjustin3738@gmail.com>
2025-06-26 15:39:42 +0530

    work

--- Dom.js
+++ Dom.js
@@ -1,6 +1,7 @@
 document.title="Dom in JavaScript";
-document.getElementById("header").innerHTML = "Death";
-document.getElementById("header").style.color = "blanchedalmond";
+document.getElementById("header").innerHTML = "Emerging";
+document.getElementById("header").style.color = "lightGreen";
+document.getElementById("header").style.backgroundcolor = "Green";
 document.getElementById("para").innerHTML = "NOTE";
 document.body.style.backgroundColor = "lightgreen";
 document.getElementById("para").style.color= "Red";
@@ -21,13 +22,33 @@ document.body.style.backgroundColor
 
 }
 function changeBackground( ){
+    // if (document.body.style.backgroundColor==="lightgreen") {
+    //     document.body.style.backgroundColor="skyblue";
+    // }
+    // else if(document.body.style.backgroundColor==="skyblue") {
+    //     document.body.style.backgroundColor="Orange";
+    // }
+    // else {
+    // document.body.style.backgroundColor="lightgreen";
+    // }
     if (document.body.style.backgroundColor==="lightgreen") {
         document.body.style.backgroundColor="skyblue";
+        document.getElementById("clg").innerHTML = "click BG !";
+        document.getElementById("clg").style.textAlign = "center";
     }
     else if(document.body.style.backgroundColor==="skyblue") {
         document.body.style.backgroundColor="Orange";
+        document.getElementById("clg").innerHTML = "Damm Why you click BG ?";
     }
-    else {
+    else if(document.body.style.backgroundColor==="Orange") {
+        document.body.style.backgroundColor="pink";
+         document.getElementById("clg").innerHTML = "NO problem click BG ";
+    }
+    else if(document.body.style.backgroundColor==="pink") {
+        document.body.style.backgroundColor="lightblue";
+    }
+    else
+    {
     document.body.style.backgroundColor="lightgreen";
     }
 }
@@ -39,7 +60,7 @@ function changeText(){
     document.getElementById("clg").style.textAlign ="center";
     document.getElementById("clg").style.border = "solid black";
     //document.getElementById("clg")
-    
+     
 }
 
 // function changeBackground(){
